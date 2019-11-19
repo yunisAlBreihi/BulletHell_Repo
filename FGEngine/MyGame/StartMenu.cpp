d55643999a358f41afb9a11552c5eaba83ec3b78
JohannesK90 <johannes.kemppainen@futuregames.nu>
2019-11-19 10:09:17 +0100

    Added starMenu class

--- FGEngine/MyGame/StartMenu.cpp
+++ FGEngine/MyGame/StartMenu.cpp
@@ -1,9 +1,11 @@
 #include "StartMenu.h"
 #include "Camera.h"
 #include "Sprite.h"
+#include "InputManager.h"
 #include <SDL_render.h>
+#include <iostream>
 
-StartMenu::StartMenu(FG::Vector2D position, FG::Sprite sprite) : sprite(sprite), position(position)
+StartMenu::StartMenu(FG::Vector2D position, FG::Sprite sprite, FG::InputManager* inputRef) :position(position), sprite(sprite), mouseInput(inputRef)
 {
 
 }
@@ -13,7 +15,19 @@ void StartMenu::Render(Renderer* const camera)
 	camera->Render(position, sprite);
 }
 
+void StartMenu::ChangeMode(int change)
+{
+	if (mouseInput-> IsMouseButtonPressed(0))
+	{
+		std::cout << "mouse 0 pressed";
+		if ((mouseInput->mousePosition.x -= position) && (x < box.x + box.w) && (y > box.y) && (y < box.y + box.h))
+		{
+
+		}
+	}
+}
+
 void StartMenu::Update(float deltaTime)
 {
-
+	
 }
--- FGEngine/MyGame/StartMenu.h
+++ FGEngine/MyGame/StartMenu.h
@@ -1,14 +1,21 @@
 #pragma once
 #include "Entity.h"
 
+namespace FG
+{
+	class InputManager;
+}
 class StartMenu : public FG::Entity
 {
 public:
 	FG::Sprite sprite;
 	FG::Vector2D position;
-	StartMenu(FG::Vector2D position, FG::Sprite sprite);
+	StartMenu(FG::Vector2D position, FG::Sprite sprite, FG::InputManager* inputRef);
+	FG::InputManager* mouseInput;
+	int selectedMode;
+
 	void Update(float deltaTime) override;
 	void Render(Renderer* const camera) override;
-
+	void ChangeMode(int change);
 };
 
