0aa51986ac7974eff54be4b20117735a270bcf29
Martin Carlsson <Martin.Carlsson@futuregames.nu>
2019-11-21 09:54:58 +0100

    Revert "basic random"
    This reverts commit 702693bf4df30f53901fb7171a74c8cafacc4dc7.

--- FGEngine/FGEngine/BasicRandom.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <random>
-class BasicRandom
-{
-public:
-	static float Range(float min, float max);
-	static int Range(int min, int max);
-};
--- FGEngine/FGEngine/main.cpp
+++ FGEngine/FGEngine/main.cpp
@@ -4,39 +4,10 @@
 #include <cassert>
 #include <SDL_main.h>
 
-#include "BasicRandom.h"
-#include <iostream>
 extern FG::Application* FG::CreateApplication();
 
 int main(int argc, char* argv[])
 {
-	for (int i = 0; i < 10; i++)
-	{
-		int v = BasicRandom::Range(0, 10);
-		std::cout << v << '\n';
-	}
-
-
-	for (int i = 0; i < 10; i++)
-	{
-		int v = BasicRandom::Range(0, 100);
-		std::cout << v << '\n';
-	}
-
-
-	for (int i = 0; i < 10; i++)
-	{
-		float v = BasicRandom::Range(0.0f, 10.0f);
-		std::cout << v << '\n';
-	}
-
-
-	for (int i = 0; i < 10; i++)
-	{
-		float v = BasicRandom::Range(0.0f, 100.0f);
-		std::cout << v << '\n';
-	}
-
 	SDL_LogSetOutputFunction(&FG::Logger::LogSDL, nullptr);
 	SDL_SetMainReady();
 	FG::Application* application = FG::CreateApplication();
